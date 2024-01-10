4b1ec34deb51c6b44b042d8b097467ef5492b311
iam-harshit <importantemail545@gmail.com>
2024-01-11 03:28:09 +0530

    Add backend url

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import Register from "./components/Register";
-import ipConfig from "./ipConfig.json";
 import { Route, Switch } from "react-router-dom";
 import Login from "./components/Login";
 import Products from "./components/Products";
@@ -7,7 +6,7 @@ import Checkout from "./components/Checkout";
 import Thanks from "./components/Thanks";
 
 export const config = {
-  endpoint: `http://${ipConfig.workspaceIp}:8082/api/v1`,
+  endpoint: "https://qkart-frontend-tcim.onrender.com",
 };
 
 function App() {
