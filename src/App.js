6889a0e752a2092b6979c197e69880e7844b06af
iam-harshit <importantemail545@gmail.com>
2024-01-11 03:36:33 +0530

    Updated react-scripts package version

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Register from "./components/Register";
+import ipConfig from "./ipConfig.json";
 import { Route, Switch } from "react-router-dom";
 import Login from "./components/Login";
 import Products from "./components/Products";
@@ -6,7 +7,7 @@ import Checkout from "./components/Checkout";
 import Thanks from "./components/Thanks";
 
 export const config = {
-  endpoint: "https://qkart-frontend-tcim.onrender.com",
+  endpoint: `http://${ipConfig.workspaceIp}:8082/api/v1`,
 };
 
 function App() {
