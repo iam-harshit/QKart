49497b76eef6d39e2c17ecab57b1a95bd80e45bb
iam-harshit <importantemail545@gmail.com>
2024-01-11 03:37:47 +0530

    Updated backend endpoint

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import Register from "./components/Register";
-import ipConfig from "./ipConfig.json";
 import { Route, Switch } from "react-router-dom";
 import Login from "./components/Login";
 import Products from "./components/Products";
 import Checkout from "./components/Checkout";
-import Thanks from "./components/Thanks";
+import Thanks from "./components/Thanks"
 
 export const config = {
-  endpoint: `http://${ipConfig.workspaceIp}:8082/api/v1`,
+  endpoint: "https://qkart-frontend-tcim.onrender.com",
 };
 
 function App() {
